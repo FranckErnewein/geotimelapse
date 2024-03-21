d546122ae42e8ba4bb5cb40b91bb514b1a715987
FranckErnewein <franck.ernewein@gmail.com>
2024-03-21 15:58:40 +0100

    init flask server
            data are static json for now

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+import json
+from flask import Flask, jsonify, abort
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify(code=404, name="Not Found", message="Ressource not found")
+
+@app.route("/ping", methods=['GET'])
+def ping():
+    return jsonify(ping="pong")
+
+with open('all_config.json') as all_config_file:
+  all_config = json.load(all_config_file)
+
+@app.route("/config/<id>", methods=['GET'])
+def config(id):
+    for config in all_config:
+        if(config['id'] == id):
+            return jsonify(config)
+    abort(404) 
+                 
