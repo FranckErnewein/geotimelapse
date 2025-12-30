a3f68dfea540b6f698966f0f57624dd127be4116
Franck Ernewein <franck.ernewein@vibe.co>
2025-12-30 09:41:21 -0500

    supress warning/error post update

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -27,6 +27,11 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
+    <RouterProvider
+      router={router}
+      future={{
+        v7_startTransition: true,
+      }}
+    />
   </QueryClientProvider>
 )
